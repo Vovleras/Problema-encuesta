6598b2aba7a09a68da0a9041913e77c1914e3754
SheilaV7 <sheila.valencia@correounivalle.edu.co>
2024-12-07 23:17:44 -0500

    Corregir tema

--- datosEntrada.py
+++ datosEntrada.py
@@ -49,25 +49,21 @@ def resultados_preguntas(temas):
     return preg_mayor, preg_menor, preg_mayor_exp, preg_menor_exp
 
 def escribir_resultado(listaTemas, encuestados):
-
+    #Encuestados con mayor y menor opinion
     enc_mayor = encuestados[0]
     enc_menor = encuestados[len(encuestados)-1]
-
+    #Encuestados con mayor y menor experticia
     ordenados_experticia = enc_orden_experticia(encuestados) #Encuestados ordenados por experticia
     enc_mayor_ex = ordenados_experticia[0]
     enc_menor_ex = ordenados_experticia[len(ordenados_experticia)-1]
-
+    #Preguntas con mayor y menor promedio de opinion y experticia
     preg_mayor, preg_menor, preg_mayor_ex, preg_menor_ex = resultados_preguntas(listaTemas)
-
+    #Promedio de opinion y experticia de los encuestados
     prom_encuestados_op = round(sum([e.opinion for e in encuestados]) / len(encuestados), 2)
     prom_encuestados_ex = round(sum([e.experticia for e in encuestados]) / len(encuestados), 2)
-
-
-
+    #Escribir en el archivo de resultados toda la infomación obtenida
     with open("resultados.txt", "w") as archivo:
-
         archivo.write("Resultados de la encuesta")
-
         for i in listaTemas:
             promedio = round(i.promedio_opinion(), 2)
             nombre = i.nombre
@@ -92,12 +88,9 @@ def escribir_resultado(listaTemas, encuestados):
         archivo.write(f"Encuestado con menor experticia: {enc_menor_ex}\n")
         archivo.write(f"Promedio de experticia de los encuestados: {prom_encuestados_ex}\n")
         archivo.write(f"Promedio del valor de opinion de los encuestados: {prom_encuestados_op}\n")
-    print(1)
 
 def ordenarPersonas(listaDesordenada):
-    #LLamar aqui al algoritmo de ordenamiento
     encuestados = []
-
     for i in listaDesordenada:
         datos = re.findall(r'\d+', i) #Se obtiene el id, la experticia y la opinion
         datosNumericos = [int(num) for num in datos] #Convertir a enteros 
@@ -123,23 +116,19 @@ def ordenarPersonas(listaDesordenada):
 def ordenarPregunta(pregunta, personas, nomPreg):
     orden = pregunta.strip("{}").split(", ")
     ids = [int(num) for num in orden]
-    #listaPersonas = personas.split("\n")
     encuestadosDesorden = []
-
     for i in ids:
         encuestadosDesorden.append(personas[i-1])
     
     encuestadosOrden = ordenarPersonas(encuestadosDesorden)
 
     preguntaLista = Pregunta(nomPreg, encuestadosOrden) #Aqui esta solo una pregunta
-    #print(preguntaLista.promedio_opinion()) #Solo es de prueba
-    #return encuestadosOrden
     return preguntaLista
 
 def ordenarTema(cadaTema, encuestados, nombre):
     nombre_tema = nombre
-    nombrePregunta = 1 #Es el nombre de la pregunta, se debe modificar segun corresponda
-    preguntasOrden = [] #Lista que contiene las preguntas ordenadas    
+    nombrePregunta = 1 
+    preguntasOrden = []    
     for i in cadaTema:
         nombre_texto = str(nombre_tema) + "." + str(nombrePregunta)
         preguntasOrden.append(ordenarPregunta(i, encuestados, nombre_texto))
@@ -155,7 +144,6 @@ def ordenarTema(cadaTema, encuestados, nombre):
     pregOrdenadas = cola_preguntas.mostrar() #Aqui estan las preguntas ordenadas en el tema
 
     temaOrdenado = Tema(nombre_tema, pregOrdenadas)
-    #print(pregOrdenadas)
     return temaOrdenado
 
 def temas(info, totalEncuestados):
@@ -163,7 +151,7 @@ def temas(info, totalEncuestados):
     for i in range(1, len(info)):
         temas.append(ordenarTema(info[i].split("\n"), totalEncuestados, i))
 
-    # Crear la cola y cargar los encuestados
+    # Crear la cola y cargar los temas
     cola_temas = ColaInsertionSort(max_length=len(temas) + 1)
     for t in temas:
         cola_temas.enqueue(t)
@@ -184,13 +172,13 @@ def asignarID(encuestadosSin):
     
     return encuestadosDesorden
 
-
 def cargarArchivo():
     nombre = input("Ingresa el nombre del archivo que deseas cargar: ")
     with open(nombre, "r") as archivo:
         contenido = archivo.read()
 
     vectorInformacion = [elemento.strip() for elemento in contenido.split("\n\n")]
+    vectorInformacion.pop()
     encuestados = asignarID(vectorInformacion[0])
     totalEOrdenados = ordenarPersonas(encuestados)
     
@@ -198,7 +186,5 @@ def cargarArchivo():
     k = len(vectorInformacion) - 2
     resultado_temas = temas(vectorInformacion, encuestados)
     escribir_resultado(resultado_temas, totalEOrdenados)
-    #escribir_promedios_op(totalEOrdenados, resultado_temas)
     
-
 cargarArchivo()
